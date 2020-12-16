46505e015cb1011f45634f70a008b62153897b80
jaorbo <74269593+jaorbo@users.noreply.github.com>
2020-12-15 19:03:21 -0500

    Add files via upload

---
+++ faraway/js/try1.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $("p1").fadeOut(1000);
+  });
+  $(".btn2").click(function(){
+    $("p1").fadeIn(1000);
+  });
+});
+
+
+
+$(document).ready(function(){
+  $(".btn3").click(function(){
+    $("p").fadeOut(1000);
+  });
+  $(".btn4").click(function(){
+    $("p").fadeIn(1000);
+  });
+});
